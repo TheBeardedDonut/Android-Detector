125df11761cde4dae33c9471d9da2a85d592e713
Chris Payne <cdp2620@g.rit.edu>
2012-08-18 14:37:48 -0400

    First layout created.

--- gen/com/example/detector/R.java
+++ gen/com/example/detector/R.java
@@ -15,7 +15,14 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int button1=0x7f070004;
+        public static final int button2=0x7f070005;
+        public static final int edit_message=0x7f070001;
+        public static final int menu_settings=0x7f070007;
+        public static final int textView1=0x7f070002;
+        public static final int textView2=0x7f070003;
+        public static final int textView3=0x7f070000;
+        public static final int textView4=0x7f070006;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -25,9 +32,11 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int button_max=0x7f040005;
+        public static final int button_on=0x7f040004;
+        public static final int edit_message=0x7f040003;
+        public static final int menu_settings=0x7f040001;
+        public static final int title_activity_main=0x7f040002;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
